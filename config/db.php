28d38dcacb337d471822e1c41600b80031e39f25
Foxy3555 <foxy_life3555@mail.ru>
2024-03-02 11:10:49 +0700

    Add registration

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'tkgbgfse_m3',
+    'username' => 'tkgbgfse',
+    'password' => '2P5h5d',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
